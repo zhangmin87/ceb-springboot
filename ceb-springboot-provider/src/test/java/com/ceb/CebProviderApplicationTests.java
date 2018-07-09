c9efe8ecb741ec8c69584acbd27d3618ba84a1a8
zhangmin <1115307804@qq.com>
2018-07-09 10:57:07 +0800

    Initial commit

---
+++ ceb-springboot-provider/src/test/java/com/ceb/CebProviderApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ceb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CebProviderApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
---
+++ src/main/java/com/examp/demo/CebSprigbootApplication.java
@@ -0,0 +1,12 @@
+package com.examp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CebSprigbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CebSprigbootApplication.class, args);
+    }
+}
