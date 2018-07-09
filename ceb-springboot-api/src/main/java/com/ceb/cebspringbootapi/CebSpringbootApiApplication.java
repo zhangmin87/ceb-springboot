7535299f9e4e31b0ffb15df6fe4fb1c0aef78277
zhangmin <1115307804@qq.com>
2018-07-09 15:48:40 +0800

    ceb-springboot 初始化

---
+++ ceb-springboot-api/src/main/java/com/ceb/cebspringbootapi/CebSpringbootApiApplication.java
@@ -0,0 +1,12 @@
+package com.ceb.cebspringbootapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CebSpringbootApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CebSpringbootApiApplication.class, args);
+    }
+}
