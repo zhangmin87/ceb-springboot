ccacfa18243b35ac965e49a4568a3b59b0c93e46
zhangmin <1115307804@qq.com>
2018-07-10 17:53:39 +0800

    ceb-springboot 初始化

--- ceb-springboot-provider/src/main/java/com/ceb/CebProviderApplication.java
+++ ceb-springboot-provider/src/main/java/com/ceb/CebProviderApplication.java
@@ -3,7 +3,7 @@ package com.ceb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
 public class CebProviderApplication {
 
     public static void main(String[] args) {
