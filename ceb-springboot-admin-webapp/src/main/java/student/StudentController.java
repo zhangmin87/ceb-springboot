617b37a4706021a90c0f7b6681af3da6dfc151a0
zhangmin <1115307804@qq.com>
2018-07-13 13:09:33 +0800

    gradle  修改 ，目录结构调整

---
+++ ceb-springboot-admin-webapp/src/main/java/student/StudentController.java
@@ -0,0 +1,13 @@
+package student;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/student")
+public class StudentController {
+//    @Reference
+//    private StudentService
+//}
+}
