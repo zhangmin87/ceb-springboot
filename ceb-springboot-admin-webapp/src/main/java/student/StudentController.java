4625279a6585574da611e79e8bce8193cdeab9d7
zhangmin <1115307804@qq.com>
2018-07-13 13:41:48 +0800

    gradle  修改 ，目录结构调整

--- ceb-springboot-admin-webapp/src/main/java/student/StudentController.java
+++ ceb-springboot-admin-webapp/src/main/java/student/StudentController.java
@@ -1,13 +1,14 @@
 package student;
 
 
+import com.alibaba.dubbo.config.annotation.Reference;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/student")
 public class StudentController {
-//    @Reference
+    @Reference
 //    private StudentService
 //}
 }
--- ceb-springboot-provider/src/main/java/com/ceb/system/service/impl/StudentServiceImpl.java
+++ ceb-springboot-provider/src/main/java/com/ceb/system/service/impl/StudentServiceImpl.java
@@ -1,10 +1,23 @@
 package com.ceb.system.service.impl;
 
+import com.alibaba.dubbo.config.annotation.Service;
+import com.ceb.system.mapper.StudentMapper;
 import com.ceb.system.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+
+@Component
+@Service(interfaceClass =StudentService.class )
 public class StudentServiceImpl implements StudentService {
+
+    @Autowired
+    private StudentMapper mapper;
+
     @Override
     public String getStudent() {
-        return null;
+       //fixme 获取student 信息
+        String stuent = mapper.select();
+        return stuent;
     }
 }
